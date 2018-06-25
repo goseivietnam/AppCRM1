0f4310681717cf8e17a8ea022e0fc8e9d7034ca4
thuld <le.duy.thu@gosei.com.vn>
2018-06-25 11:30:51 +0700

    fix

--- AppCRM/AppCRM/Services/Request/RequestService.cs
+++ AppCRM/AppCRM/Services/Request/RequestService.cs
@@ -23,7 +23,7 @@ namespace AppCRM.Services.Request
     }
     public class RequestService : IRequestService
     {
-        public static readonly string HOST_NAME = "http://3e080134.ngrok.io/";
+        public static readonly string HOST_NAME = "http://4a5b04d0.ngrok.io/";
         public static string ACCESS_TOKEN;
 
         public async Task<dynamic> getDataFromService(string queryString)
