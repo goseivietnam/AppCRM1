0986d69ae0554973546c495de550f10581ca9a1a
ThangUC <thanguc.94@outlook.com>
2018-08-07 18:01:50 +0700

    Fix iOS incompatible

---
+++ AppCRM/AppCRM.Android/Renderer/SJTabbedPageRenderer.cs
@@ -0,0 +1,111 @@
+﻿using Android.Content;
+using Android.Support.Design.Internal;
+using Android.Support.Design.Widget;
+using Android.Views;
+using AppCRM.Controls;
+using AppCRM.Droid.Renderer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android.AppCompat;
+using View = Android.Views.View;
+
+[assembly: ExportRenderer(typeof(SJTabbedPage), typeof(SJTabbedPageRenderer))]
+namespace AppCRM.Droid.Renderer
+{
+    public class SJTabbedPageRenderer : TabbedPageRenderer
+    {
+        private bool _isShiftModeSet;
+
+        public SJTabbedPageRenderer(Context context): base(context)
+        {
+        }
+
+        protected override void OnLayout(bool changed, int l, int t, int r, int b)
+        {
+            base.OnLayout(changed, l, t, r, b);
+            try
+            {
+                if (!_isShiftModeSet)
+                {
+                    var children = GetAllChildViews(ViewGroup);
+
+                    if (children.SingleOrDefault(x => x is BottomNavigationView) is BottomNavigationView bottomNav)
+                    {
+                        bottomNav.SetShiftMode(false, false);
+                        _isShiftModeSet = true;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error setting ShiftMode: {e}");
+            }
+        }
+
+        private List<View> GetAllChildViews(View view)
+        {
+            if (!(view is ViewGroup group))
+            {
+                return new List<View> { view };
+            }
+
+            var result = new List<View>();
+
+            for (int i = 0; i < group.ChildCount; i++)
+            {
+                var child = group.GetChildAt(i);
+
+                var childList = new List<View> { child };
+                childList.AddRange(GetAllChildViews(child));
+
+                result.AddRange(childList);
+            }
+
+            return result.Distinct().ToList();
+        }
+    }
+
+    public static class AndroidHelpers
+    {
+        public static void SetShiftMode(this BottomNavigationView bottomNavigationView, bool enableShiftMode, bool enableItemShiftMode)
+        {
+            try
+            {
+                var menuView = bottomNavigationView.GetChildAt(0) as BottomNavigationMenuView;
+                if (menuView == null)
+                {
+                    System.Diagnostics.Debug.WriteLine("Unable to find BottomNavigationMenuView");
+                    return;
+                }
+
+
+                var shiftMode = menuView.Class.GetDeclaredField("mShiftingMode");
+
+                shiftMode.Accessible = true;
+                shiftMode.SetBoolean(menuView, enableShiftMode);
+                shiftMode.Accessible = false;
+                shiftMode.Dispose();
+
+
+                for (int i = 0; i < menuView.ChildCount; i++)
+                {
+                    var item = menuView.GetChildAt(i) as BottomNavigationItemView;
+                    if (item == null)
+                        continue;
+
+                    item.SetShiftingMode(enableItemShiftMode);
+                    item.SetChecked(item.ItemData.IsChecked);
+
+                }
+
+                menuView.UpdateMenuView();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Unable to set shift mode: {ex}");
+            }
+        }
+    }
+}
---
+++ AppCRM/AppCRM/Controls/SJTabbedPage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AppCRM.Controls
+{
+    public class SJTabbedPage : TabbedPage
+    {
+        public static readonly BindableProperty SelectedIndexProperty = BindableProperty.Create(nameof(SelectedIndex), typeof(int), typeof(SJTabbedPage), 0, propertyChanged: (page, oldValue, newValue) =>
+        {
+            var tabbedPage = (SJTabbedPage)page;
+            if(tabbedPage != null && tabbedPage.Children != null && tabbedPage.Children.Count > (int)newValue)
+            {
+                tabbedPage.CurrentPage = tabbedPage.Children[(int)newValue];
+            }
+        });
+
+        public int SelectedIndex
+        {
+            get
+            {
+                return (int)GetValue(SelectedIndexProperty);
+            }
+            set
+            {
+                SetValue(SelectedIndexProperty, value);
+            }
+        }
+    }
+}
